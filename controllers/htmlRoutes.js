9379e48db0308b440a87350fbc9b06eea5722965
Victoria League <vleague2@gmail.com>
2018-05-15 19:36:18 -0400

    made error page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,8 +1,16 @@
+// NODE MODULES
 const express = require('express');
 const router = express.Router();
 
+// HOME PAGE ROUTE
 router.get('/', (req, res) => {
     res.render('index');
 })
 
+// PAGE NOT FOUND
+router.get('*', (req, res) => {
+    res.render('error');
+})
+
+// EXPORT FOR SERVER
 module.exports = router;
