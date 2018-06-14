c347e5fa5694800613544119b4c8e3623c76d3e0
unknown <vleague2@gmail.com>
2018-06-14 14:44:16 -0400

    mobile responsive

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -7,6 +7,11 @@ router.get('/', (req, res) => {
     res.render('index');
 })
 
+// in progress
+// router.get('/project/novelize', (req, res) => {
+//     res.render('project');
+// })
+
 // PAGE NOT FOUND
 router.get('*', (req, res) => {
     res.render('error');
