1da8d0802f0682345abdcb4ca853be7aa09b8012
Victoria League <vleague2@gmail.com>
2018-06-25 17:06:25 -0400

    working on project page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,17 +1,17 @@
 // NODE MODULES
 const express = require('express');
 const router = express.Router();
-const project_previews = require('./../data/projectpreviews.json');
+const projects = require('./../data/projects.json');
 const timeline_items = require("./../data/timelineItems.json")
 
 // HOME PAGE ROUTE
 router.get('/', (req, res) => {
-    res.render('index', {projects: project_previews, timelineItems: timeline_items});
+    res.render('index', {projects: projects, timelineItems: timeline_items});
 })
 
 // in progress
 // router.get('/project/novelize', (req, res) => {
-//     res.render('project');
+//     res.render('project', {project: projects[0]});
 // })
 
 // PAGE NOT FOUND
